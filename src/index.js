753124edc629f2d16748061f91946bcb3bf9d120
Mays Jones <maysjones2@gmail.com>
2022-04-13 11:57:45 -0400

    commit tree 393623ba3291225e383a1e0de2430026f2833ee2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-// write your code here
+const ramenMenu = document.querySelector('#ramen-menu')
+console.log(ramenMenu)
+
+const baseUrl= 'http://localhost:3000/'
+fetch(baseUrl + 'ramens')
+.then((response)=>response.json())
+.then((ramensData)=>renderRamens(ramensData))
+
+function renderRamens(ramens){
+    ramens.forEach(ramen =>{
+        const img= document.createElement('img')
+        img.src = ramen.image
+        img.details = ramen
+        img.addEventListener('click', updateRamenDetails)
+        ramenMenu.append(img)
+    })
+}
+
+function updateRamenDetails(event){
+    const name = document.querySelector('.name')
+    const image = document.querySelector('.detail-image')
+        image.src = event.target.details.image
+    const restaurant = document.querySelector(".restaurant")
+    name.replace("Insert Name Here", event.target.details.name)
+    image.appendChild(event.target.details.image)
+    restaurant.replace("Insert Restaurant Here", event.target.details.restaurant)
+}
+const ramenForm = document.querySelector('#new-ramen')
+ramenForm.addEventListener('submit', createRamen)
+function createRamen(e){e.preventDefault()
+    const name =document.querySelector('#new-name').value
+    const comment = document.querySelector('#new-comment').value
+    const ramen = {name,restauraunt,image,rating,comment}
+
+}
